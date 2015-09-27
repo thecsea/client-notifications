94fbe525b1b8214e82796c65772c634fb8697d84
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-09-28 01:48:15 +0200

    just a comment

--- src/NotificationManager.php
+++ src/NotificationManager.php
@@ -93,7 +93,7 @@ class NotificationManager
      */
     public function getDbConnection()
     {
-        //DON'T clone
+        //DON'T clone to keep mysql connection number efficiently
         return $this->dbConnection;
     }
 
